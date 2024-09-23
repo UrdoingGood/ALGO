359b4ce14eb9674acdf7dd6a36f9db873a28c1bd
UrdoingGood <1222pgy@naver.com>
2024-09-24 07:58:11 +0900

    집 0924

--- BOJ/나이트의 이동/7562.py
+++ BOJ/나이트의 이동/7562.py
@@ -3,20 +3,22 @@ sys.stdin = open('input.txt', 'r')
 from collections import deque
 
 dxy = [(-2,-1), (-1,-2), (-2,1), (-1,2), (1,-2), (2,-1), (2,1), (1,2)]
-def bfs(x, y):
+def bfs(x, y, cnt):
     global visited
-    count = 0
+    global count
     queue = deque()
-    queue.append((x, y))
+    queue.append((x, y, cnt))
+    visited[x][y] = True
     while queue:
-        cx, cy = queue.popleft()
-        if (cx, cy) == (ex, ey):
+        cx, cy, count = queue.popleft()
+        if (cx, cy) == (ex, ey): # 이동하려는 칸에 도달하면 끝
             return count
         for dx, dy in dxy:
             nx, ny = cx + dx, cy + dy
             if 0 <= nx < N and 0 <= ny < N and visited[nx][ny] == False:
-                count += 1
-                queue.append((nx, ny))
+                # 레벨 단위로 계산하기 때문에 count도 함께 넘겨주기!
+                # 처음에는 count += 1을 따로 작성해버렸음.
+                queue.append((nx, ny, count + 1))
                 visited[nx][ny] = True
     return count
 
@@ -24,7 +26,7 @@ T = int(input())
 for test_case in range(T):
     N = int(input())
     visited = [[False] * N for _ in range(N)]
+    count = 0
     sx, sy = map(int, input().split())
     ex, ey = map(int, input().split())
-
-    print(bfs(sx, sy))
+    print(bfs(sx, sy, count))
