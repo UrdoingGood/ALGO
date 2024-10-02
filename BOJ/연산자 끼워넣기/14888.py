0b666f3c6f9971d2e4317f0b112f4a9367bac70a
UrdoingGood <1222pgy@naver.com>
2024-10-02 17:56:52 +0900

    강의실 1002

--- BOJ/연산자 끼워넣기/14888.py
+++ BOJ/연산자 끼워넣기/14888.py
@@ -1,7 +1,35 @@
 import sys
 sys.stdin = open('input.txt', 'r')
 
-N = int(input())
-A = [map(int, input().split(' '))]
-op = [map(int, input().split(' '))]
+def dfs(n, sm, add, sub, mul, div):
+    global mn, mx
 
+    if n == N:
+        mn = min(sm, mn)
+        mx = max(sm, mx)
+        return
+
+    if sm < int(-1e8) or sm > int(1e8):
+        return
+
+    if add > 0:
+        dfs(n+1, sm+A[n], add-1, sub, mul, div)
+    if sub > 0:
+        dfs(n+1, sm-A[n], add, sub-1, mul, div)
+    if mul > 0:
+        dfs(n+1, sm*A[n], add, sub, mul-1, div)
+    if div > 0:
+        if sm < 0:
+            dfs(n + 1, -(abs(sm)//A[n]), add, sub, mul, div - 1)
+        else:
+            dfs(n + 1, int(sm//A[n]), add, sub, mul, div - 1)
+
+N = int(input())
+A = list(map(int, input().split(' ')))
+add, sub, mul, div = map(int, input().split(' '))
+mn = int(1e8)
+mx = int(-1e8)
+
+dfs(1, 3, add, sub, mul, div)
+print(mx)
+print(mn)
